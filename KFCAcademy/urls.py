394bc8a180909930dcd0ad7102df251b509f3685
Collins Munene <hillarycollins@protonmail.com>
2026-04-27 18:11:19 +0300

    feat:update healthz

--- KFCAcademy/urls.py
+++ KFCAcademy/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('health/',HealthCheckView.as_view(),name='health_check'),
+    path('api/health/',HealthCheckView.as_view(),name='health_check'),
     path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
